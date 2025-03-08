d95b9eed0fc10777ddbcc714bb0e078de9a6800a
iintii <intihoss@gmail.com>
2025-03-08 13:59:34 -0500

    new_commit

--- .gitignore/p8 - Trie and Pointers/pointer.py
+++
@@ -1,7 +0,0 @@
-def move_zeroes_to_the_end(alist):
-    p1 = 0
-    for p2 in range(len(alist)):
-        if alist[p2] != 0: 
-            alist[p1] = alist[p2]
-            p1 += 1 #Increment p1 only when its non zero
-    return alist
