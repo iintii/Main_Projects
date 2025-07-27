d77a2aca8095a185f511dae0111d1ae83461aa23
iintii <intihoss@gmail.com>
2025-07-27 17:42:16 -0400

    727 removedChef

--- AllReactProjects/ReactTravelJournal/TravelJournal/src/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <header>
-      <img src=".\src\assets\Globe.png" alt="globe-img" />
-      <span>my travel journal.</span>
-    </header>
-  );
-}
